2bf4b0ec99e9dc97c77093bd86575e32b06d2bee
mrgalvipro <kblack723@hotmail.com>
2026-08-25 08:47:38 -0400

    Day 1: wire Pre-Founder Human E2E continuity

--- worker/day1.js
+++ worker/day1.js
@@ -1,3 +1,3 @@
 // GalviStudio | GalviCare 1.0 Day 1 QA entry; legacy P0 behavior delegates through the governed wrapper.
 export { API_VERSION, FIXTURE, REQUIRED_SCHEMA, SERVICE, TABLE_PREFIX, GALVICARE_RELEASE, GALVICARE_SCHEMA } from './day1-galvicare-1-0.js';
-export { default } from './day1-galvicare-1-0.js';
+export { default } from './day1-human-e2e.js';
